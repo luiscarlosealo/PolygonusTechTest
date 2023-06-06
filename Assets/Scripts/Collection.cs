aae0ee9ebfcecc86742f242dc2bb8f3dce8e5455
Luis Carlos Ealo <luiscarlosealo@gmail.com>
2023-06-06 18:12:59 -0500

    Particles and Collection

---
+++ Assets/Scripts/Collection.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection : MonoBehaviour
+{
+    public GameObject collectionEffect;
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Coin") 
+        {
+
+            Instantiate(collectionEffect, other.transform.position, other.transform.rotation);
+           
+            Destroy(other.gameObject); 
+
+        }
+    }
+}
